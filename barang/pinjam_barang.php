897ae564585f9d14b84ddecb00e171a501d740a7
Felix Ferdianto <77229194+xileferdianto08@users.noreply.github.com>
2022-12-03 00:37:44 +0700

    progress

--- barang/pinjam_barang.php
+++ barang/pinjam_barang.php
@@ -3,14 +3,56 @@ if($_SERVER['REQUEST_METHOD'] =='POST'){
     require_once '../dbConfig/db_connect.php';
 
     if($con) {
-        
+        $userId = $_POST['userId'];
+        $barangId = $_POST['barangId'];
+        $startDate = $_POST['startDate'];
+        $startTime = $_POST['startTime'];
+        $endDate = $_POST['endDate'];
+        $endTime = $_POST['endTime'];
+        $qty = $_POST['qty'];
+        $necessity = $_POST['necessity'];
+        $response = array();
 
+        $query = "SELECT * FROM `peminjaman_barang` WHERE `barangId` = '$barangId'";
+        $result = mysqli_query($con, $query);
 
+        $row = mysqli_num_rows($result);
+        $results = mysqli_fetch_all($result);
+
+        $sisaBarang = $results['qty'] - $qty;
+
+        if($sisaBarang < 1){
+            http_response_code(400);
+            array_push($response, array(
+                'status' => 'NO_ITEMS_LEFT'
+            ));
+        } else if ($qty > $results['qty']){
+            http_response_code(400);
+            array_push($response, array(
+                'status' => 'EXCEED_MAX_QTY'
+            ));
+        } else {
+            $query2 = "INSERT INTO `peminjaman_barang`(`id`, `userId`, `barangId`, `startDate`, `startTime`, `endDate`, `endTime`, `qty`, `necessity`, `status`) VALUES (NULL,'$userId','$barangId','$startDate','$startTime','$endDate','$endTime','$qty','$necessity',NULL)";
+
+            $result2 = mysqli_query($con, $query2);
+
+            if ($result2) {
+                http_response_code(200);
+                array_push($response, array(
+                    'status' => 'OK'
+                ));
+            } else {
+                http_response_code(200);
+                array_push($response, array(
+                    'status' => 'FAILED'
+                ));
+            }
+        }
     }else {
         http_response_code(500);
         echo "Database cannot connect";
         array_push($response, array(
-            'status' => 'DB FAILED'
+            'status' => 'DB_FAILED'
         ));
     }
 
@@ -18,4 +60,3 @@ if($_SERVER['REQUEST_METHOD'] =='POST'){
     mysqli_close($con);
     
 }
-?>
--- ruangan/pinjam_ruangan.php
+++ ruangan/pinjam_ruangan.php
@@ -1,7 +1,55 @@
 <?php 
-
 if($_SERVER['REQUEST_METHOD'] =='POST'){
     require_once '../dbConfig/db_connect.php';
+
+    if($con) {
+        $userId = $_POST['userId'];
+        $ruangId = $_POST['ruanganId'];
+        $startDate = $_POST['startDate'];
+        $startTime = $_POST['startTime'];
+        $endDate = $_POST['endDate'];
+        $endTime = $_POST['endTime'];
+        $capacity = $_POST['capacity'];
+        $necessity = $_POST['necessity'];
+        $response = array();
+
+        $query = "SELECT * FROM `peminjaman_ruang` WHERE `ruangId` = '$ruangId'";
+        $result = mysqli_query($con, $query);
+
+        $row = mysqli_num_rows($result);
+        $results = mysqli_fetch_all($result);
+
+        if($row > 0){
+            http_response_code(400);
+            array_push($response, array(
+                'status' => 'ROOM_BORROWED_ALR'
+            ));
+        } else {
+            $query2 = "INSERT INTO `peminjaman_ruang`(`id`, `userId`, `ruangId`, `startDate`, `startTime`, `endDate`, `endTime`, `capacity`, `necessity`, `status`) VALUES (NULL,'$userId','$ruangId','$startDate','$startTime','$endDate','$endTime','$capacity','$necessity',NULL)";
+
+            $result2 = mysqli_query($con, $query2);
+
+            if ($result2) {
+                http_response_code(200);
+                array_push($response, array(
+                    'status' => 'OK'
+                ));
+            } else {
+                http_response_code(200);
+                array_push($response, array(
+                    'status' => 'FAILED'
+                ));
+            }
+        }
+    }else {
+        http_response_code(500);
+        echo "Database cannot connect";
+        array_push($response, array(
+            'status' => 'DB_FAILED'
+        ));
+    }
+
+    echo json_encode(array('server_response' => $response));
+    mysqli_close($con);
     
 }
-?>
